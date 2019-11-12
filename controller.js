6a40156584b7b90bb6402af3ff41b4cc13eca775
Student <student@makerss-mbp.home>
2019-11-12 18:33:54 +0000

    Added Filter to the list

--- controller.js
+++ controller.js
@@ -2,11 +2,25 @@
 app.controller("myCtrl", function($scope) {
 
     $scope.firstName = "";
-    $scope.lastName = "Doe";
-    $scope.quantity = "1"
-    $scope.price = "1"
+    $scope.surname = "";
+    $scope.test = "test SCOPE (inside ng-app and inside ng-controller)"
+    $scope.fullName = function(){
+        return  $scope.firstName + " " + $scope.surname; 
+    };
+    $scope.quantity = "1";
+    $scope.price = "1";
     
-    $scope.names = ['Jani','Hege', 'firstName']
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Carl',country:'Sweden'},
+        {name:'Margareth',country:'England'},
+        {name:'Hege',country:'Norway'},
+        {name:'Joe',country:'Denmark'},
+        {name:'Gustav',country:'Sweden'},
+        {name:'Birgit',country:'Denmark'},
+        {name:'Mary',country:'England'},
+        {name:'Kai',country:'Norway'}
+        ];
 
 });
 
--- myApp.js
+++ myApp.js
@@ -1,1 +1,4 @@
 const app = angular.module("myApp", []);
+app.run(function($rootScope){
+    $rootScope.test = "test root SCOPE(inside ng-app outside ng-controller)"
+})
