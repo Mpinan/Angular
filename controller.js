1cadf3160123e9d5104f8d9e5deebcb344efcf8c
Student <student@Makerss-MacBook-Pro.local>
2019-11-13 09:49:08 +0000

    Added scope

--- controller.js
+++ controller.js
@@ -1,31 +1,34 @@
-
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function ($scope) {
 
     $scope.firstName = "";
     $scope.surname = "";
     $scope.test = "test SCOPE (inside ng-app and inside ng-controller)"
-    $scope.fullName = function(){
-        return  $scope.firstName + " " + $scope.surname; 
+    $scope.fullName = function () {
+        return $scope.firstName + " " + $scope.surname;
     };
-    $scope.quantity = "1";
-    $scope.price = "1";
-    
+
+    $scope.quantity = 5;
+    $scope.price = 5;
+
     $scope.names = [
-        {name:'Jani',country:'Norway'},
-        {name:'Carl',country:'Sweden'},
-        {name:'Margareth',country:'England'},
-        {name:'Hege',country:'Norway'},
-        {name:'Joe',country:'Denmark'},
-        {name:'Gustav',country:'Sweden'},
-        {name:'Birgit',country:'Denmark'},
-        {name:'Mary',country:'England'},
-        {name:'Kai',country:'Norway'}
-        ];
+        { name: 'Jani', country: 'Norway' },
+        { name: 'Carl', country: 'Sweden' },
+        { name: 'Margareth', country: 'England' },
+        { name: 'Hege', country: 'Norway' },
+        { name: 'Joe', country: 'Denmark' },
+        { name: 'Gustav', country: 'Sweden' },
+        { name: 'Birgit', country: 'Denmark' },
+        { name: 'Mary', country: 'England' },
+        { name: 'Kai', country: 'Norway' }
+    ];
+    $scope.orderByMe = function (x) {
+        $scope.myOrderBy = x;
+    }
 
 });
 
 
-app.directive('submitDirective', function(){
+app.directive('submitDirective', function () {
     return {
         template: '<button>Submit</button>'
     }
--- myApp.js
+++ myApp.js
@@ -1,4 +1,10 @@
 const app = angular.module("myApp", []);
-app.run(function($rootScope){
+app.run(function($rootScope, $interval){
     $rootScope.test = "test root SCOPE(inside ng-app outside ng-controller)"
+
+    $rootScope.theTime = new Date().toLocaleTimeString();
+    $interval(function () {
+        $rootScope.theTime = new Date().toLocaleTimeString();
+    }, 1000);
+    
 })
